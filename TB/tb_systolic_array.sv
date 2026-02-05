277ffbbace1b74ac97fbd2146661a08dcf56b097
Monesh-P <www.monesh2005@gmail.com>
2026-02-05 12:43:42 +0530

    check

--- TB/tb_systolic_array.sv
+++ TB/tb_systolic_array.sv
@@ -64,7 +64,7 @@ module tb_systolic_array;
                 B_in[j] = (t-j>=0 && t-j<N) ? B[t-j][j] : 0;
         end
 
-        repeat (2*N) @(posedge clk);
+        repeat (3*N) @(posedge clk);
 
         // Output matrix
         $display("RESULT_BEGIN");
