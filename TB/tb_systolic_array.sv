e68f7e3ae4ca23b4ae62c92801d5668e572c91c8
Monesh-P <www.monesh2005@gmail.com>
2026-02-05 14:02:00 +0530

    back

--- TB/tb_systolic_array.sv
+++ TB/tb_systolic_array.sv
@@ -86,12 +86,14 @@ module tb_systolic_array;
         // -----------------------------
         // Print Output
         // -----------------------------
-        $display("\n===== MATRIX C (A x B) =====");
+        $display("RESULT_BEGIN");
         for (i = 0; i < N; i++) begin
-            for (j = 0; j < N; j++)
-                $write("%6d ", C[i][j]);
+            for (j = 0; j < N; j++) begin
+                $write("%0d ", C[i][j]);
+            end
             $write("\n");
         end
+        $display("RESULT_END");
         $display("===========================\n");
 
         $finish;
